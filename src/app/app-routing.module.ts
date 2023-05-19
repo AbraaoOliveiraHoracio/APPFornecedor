d4f01d48393bd4615e76b313fa5ec41c384ca5ec
Abraao <aohdigital@hotmail.com>
2023-05-19 15:11:36 -0300

    Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { HeaderComponent } from './header/header.component';
+import { SupplierComponent } from './supplier/supplier.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path : 'home', component: HomeComponent},
+{path : 'supplier', component: SupplierComponent},
+//{path : 'header', component: HeaderComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
