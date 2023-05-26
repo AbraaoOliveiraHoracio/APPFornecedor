0a17f0ed954a1bc719a836b89336d0e1ba24b8d5
Abraao <aohdigital@hotmail.com>
2023-05-26 15:52:44 -0300

    Dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ClientComponent } from './client/client.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/supplier/supplier.component.ts
+++ src/app/supplier/supplier.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { Fornecedor } from '../Fornecedor';
 import { FornecedorService } from '../fornecedor.service';
 
